c97b8f538480671c43b7fba51f4e99c6f4a534c7
Jan Janech <jan.janech@fri.uniza.sk>
2023-10-03 09:21:42 +0200

    02 - projekt datum vygenerovany podla UML

---
+++ datum/Datum.java
@@ -0,0 +1,25 @@
+public class Datum {
+    private int den;
+    private int mesiac;
+    private int rok;
+    
+    public Datum(int den, int mesiac, int rok) {
+        
+    }
+    
+    public void vypis() {
+        
+    }
+    
+    public int getDen() {
+        
+    }
+    
+    public int getMesiac() {
+        
+    }
+    
+    public int getRok() {
+        
+    }
+}
