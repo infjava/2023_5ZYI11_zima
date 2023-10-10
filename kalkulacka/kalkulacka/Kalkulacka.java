2a9520a1112007884908fca7f14aea3930e5423b
Jan Janech <jan.janech@fri.uniza.sk>
2023-10-10 08:45:24 +0200

    03 - prva verzia kalkulacky

---
+++ kalkulacka/kalkulacka/Kalkulacka.java
@@ -0,0 +1,46 @@
+public class Kalkulacka {
+    private double vysledok;
+    private char operacia;
+    
+    public Kalkulacka() {
+        this.vysledok = 0;
+        this.operacia = '+';
+    }
+    
+    public void zadajCislo(double cislo) {
+        switch (this.operacia) {
+            case '+':
+                this.vysledok = this.vysledok + cislo;
+                break;
+            case '-':
+                this.vysledok = this.vysledok - cislo;
+                break;
+            case '*':
+                this.vysledok = this.vysledok * cislo;
+                break;
+            case '/':
+                this.vysledok = this.vysledok / cislo;
+                break;
+        }
+    }
+    
+    public void scitaj() {
+        this.operacia = '+';
+    }
+    
+    public void odcitaj() {
+        this.operacia = '-';
+    }
+    
+    public void vynasob() {
+        this.operacia = '*';
+    }
+    
+    public void vydel() {
+        this.operacia = '/';
+    }
+    
+    public double getVysledok() {
+        return this.vysledok;
+    }
+}
