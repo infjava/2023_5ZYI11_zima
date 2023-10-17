d73fd9064cfa85aece0314e1b19d3b105ea1f37d
Jan Janech <jan.janech@fri.uniza.sk>
2023-10-17 08:16:39 +0200

    04 - exportovany segment z UML .FRI do bluej

---
+++ cislica/cislica/Segment.java
@@ -0,0 +1,14 @@
+public class Segment {
+    
+    public Segment(int x, int y, boolean jeVertikalny) {
+        
+    }
+    
+    public void zapni() {
+        
+    }
+    
+    public void vypni() {
+        
+    }
+}
