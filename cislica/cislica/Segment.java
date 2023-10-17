140a55ab7b367ecf5969a298cfc023ff544c56b2
Jan Janech <jan.janech@fri.uniza.sk>
2023-10-17 08:42:57 +0200

    04 - doprogramovany segment

--- cislica/cislica/Segment.java
+++ cislica/cislica/Segment.java
@@ -1,14 +1,24 @@
 public class Segment {
+    private Obdlznik obdlznik;
     
     public Segment(int x, int y, boolean jeVertikalny) {
+        this.obdlznik = new Obdlznik();
         
+        this.obdlznik.posunVodorovne(x - 60);
+        this.obdlznik.posunZvisle(y - 50);
+        
+        if (jeVertikalny) {
+            this.obdlznik.zmenStrany(5, 50);
+        } else {
+            this.obdlznik.zmenStrany(50, 5);
+        }
     }
     
     public void zapni() {
-        
+        this.obdlznik.zobraz();
     }
     
     public void vypni() {
-        
+        this.obdlznik.skry();
     }
 }
