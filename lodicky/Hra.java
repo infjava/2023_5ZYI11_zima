cac55c902a38552bc63c31e8bf76302ba5738163
Jan Janech <jan.janech@fri.uniza.sk>
2023-11-21 09:36:11 +0100

    08 - vytvaranie lodiciek podla planu

--- lodicky/Hra.java
+++ lodicky/Hra.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Hra {
     private Policko[][] hraciaPlocha;
+    private Lodka[] lodky;
     
     public Hra(int velkostPolicka, String nazovMapy) throws IOException {
         File subor = new File(nazovMapy + ".txt");
@@ -11,12 +12,24 @@ public class Hra {
         
         int sirka = citac.nextInt();
         int vyska = citac.nextInt();
+        int pocetLodiek = citac.nextInt();
+        
+        this.lodky = new Lodka[pocetLodiek];
+        
+        for (int i = 0; i < pocetLodiek; i++) {
+            this.lodky[i] = new Lodka();
+        }
         
         this.hraciaPlocha = new Policko[vyska][sirka];
         
         for (int riadok = 0; riadok < vyska; riadok++) {
             for (int stlpec = 0; stlpec < sirka; stlpec++) {
                 this.hraciaPlocha[riadok][stlpec] = new Policko(stlpec, riadok, velkostPolicka);
+                
+                int priradenaLodka = citac.nextInt();
+                if (priradenaLodka != 0) {
+                    this.hraciaPlocha[riadok][stlpec].priradLodku(this.lodky[priradenaLodka - 1]);
+                }
             }
         }
         
--- lodicky/Policko.java
+++ lodicky/Policko.java
@@ -2,11 +2,18 @@ import fri.shapesge.Stvorec;
 
 public class Policko {
     private Stvorec stvorec;
+    private Lodka lodka;
     
     public Policko(int x, int y, int velkost) {
         this.stvorec = new Stvorec(x * (velkost + 1), y * (velkost + 1));
         this.stvorec.zmenStranu(velkost);
         this.stvorec.zobraz();
+        this.lodka = null;
+        this.prekresli();
+    }
+    
+    public void priradLodku(Lodka lodka) {
+        this.lodka = lodka;
         this.prekresli();
     }
     
