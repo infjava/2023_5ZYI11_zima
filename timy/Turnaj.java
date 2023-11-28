d048984f115f7066458702b4a04fa23bc2d2be32
Jan Janech <jan.janech@fri.uniza.sk>
2023-11-28 09:33:54 +0100

    09 - vypis tabulky

--- timy/Turnaj.java
+++ timy/Turnaj.java
@@ -24,4 +24,21 @@ public class Turnaj {
         
         this.uzavrety = false;
     }
+    
+    public void vypisTabulky() {
+        System.out.format("%25s", "");
+        for (Tim timHosti : this.prihlaseneTimy) {
+            System.out.format("%25s", timHosti.getNazovTimu());
+        }
+        System.out.println();
+        
+        for (int domaci = 0; domaci < this.prihlaseneTimy.length; domaci++) {
+            System.out.format("%25s", this.prihlaseneTimy[domaci].getNazovTimu());
+            
+            for (int hostia = 0; hostia < this.prihlaseneTimy.length; hostia++) {
+                System.out.format("%25s", this.tabulka[domaci][hostia]);
+            }
+            System.out.println();
+        }
+    }
 }
