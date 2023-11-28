56aa4700ff7c6ae6f9e50dc0b783fbbee7659e54
Jan Janech <jan.janech@fri.uniza.sk>
2023-11-28 09:32:09 +0100

    09 - enum Vysledok

---
+++ timy/Vysledok.java
@@ -0,0 +1,30 @@
+
+/**
+ * Enumeration class Vysledok - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Vysledok {
+    VYHRA(3, 0),
+    PREHRA(0, 3),
+    REMIZA(1, 1),
+    NEHRA_SA(0, 0),
+    NEDOHRANE(0, 0);
+    
+    private int bodyDomaci;
+    private int bodyHostia;
+    
+    Vysledok(int bodyDomaci, int bodyHostia) {
+        this.bodyDomaci = bodyDomaci;
+        this.bodyHostia = bodyHostia;
+    }
+    
+    public int getBodyDomaci() {
+        return this.bodyDomaci;
+    }
+    
+    public int getBodyHostia() {
+        return this.bodyHostia;
+    }
+}
